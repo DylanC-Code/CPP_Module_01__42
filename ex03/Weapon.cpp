ef4573a378cd61cadfc12e7a3627069278fb1986
dcastor <dcastor@student.42.fr>
2025-07-16 11:15:14 +0200

    feat(ex03): Weapon finished

--- ex03/Weapon.cpp
+++ ex03/Weapon.cpp
@@ -1,11 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 11:12:52 by dcastor           #+#    #+#             */
+/*   Updated: 2025/07/16 11:13:06 by dcastor          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Weapon.hpp"
+
+Weapon::Weapon(std::string type) : type(type) {};
+
+Weapon::~Weapon() {};
 
 const std::string &Weapon::getType() const
 {
     return (this->type);
 }
 
-void Weapon::setType(const std::string &type)
+void Weapon::setType(std::string type)
 {
     this->type = type;
 }
--- ex03/Weapon.hpp
+++ ex03/Weapon.hpp
@@ -6,7 +6,7 @@
 /*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 10:35:25 by dcastor           #+#    #+#             */
-/*   Updated: 2025/07/16 10:43:26 by dcastor          ###   ########.fr       */
+/*   Updated: 2025/07/16 11:13:00 by dcastor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,10 @@ private:
     std::string type;
 
 public:
+    Weapon(std::string type);
+    ~Weapon();
     const std::string &getType() const;
-    void setType(const std::string &name);
+    void setType(std::string name);
 };
 
 #endif
